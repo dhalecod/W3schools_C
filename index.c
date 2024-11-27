880b857569a85e5b3cccf3eec0df040ee13b68d6
dhalecod <williammakinde408@gmail.com>
2024-11-27 10:12:43 +0000

    updated

--- index.c
+++ index.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
     printf("I am Dev_Marcus\n");
-    printf('My hubbies are coding, gyming, singing and studying');
+    printf("My hubbies are coding, gyming, singing and studying");
     return 0;
 }

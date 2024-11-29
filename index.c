41b7abfffa78a716445b579ea9a4ea3af045f98b
dhalecod <williammakinde408@gmail.com>
2024-11-29 09:41:54 +0000

    updated

--- index.c
+++ index.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
-    printf("     CALCULATE THE AREA OF RECTANGLE");
+    printf("     CALCULATE THE AREA OF RECTANGLE\n");
     int lenghtOfTriangle = 5;
     int widthOfTriangle = 6;
     int areaOfTriangle = lenghtOfTriangle * widthOfTriangle;
-    printf("Area of Rectangle = lenght x width");
-    printf("if lenght of Rectangle is %d and width of Rectangle is %d", lenghtOfTriangle, widthOfTriangle);
-    printf("Area of Rectangle is %d", areaOfTriangle);
+    printf("Area of Rectangle = lenght x width\n");
+    printf("If lenght of Rectangle is %d\n And width of Rectangle is %d\n", lenghtOfTriangle, widthOfTriangle);
+    printf("Area of Rectangle is %d\n", areaOfTriangle);
     return 0;
 }

d0d30168e2270d5e8c55af03ee9802a396f03a4d
dhalecod <williammakinde408@gmail.com>
2024-11-29 09:33:32 +0000

    updated

--- index.c
+++ index.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main() {
-    char studentName[] = "Makinde Williams";
-    int studentId = 3110;
-    int studentAge = 22;
-    char studentGrade = 'A';
-    float studentScore = 99.9;
-
-    printf("Student name is %s\n", studentName);
-    printf("Student ID is %d\n", studentId);
-    printf("Student Age is %d\n", studentAge);
-    printf("StudentGrade is %c\n", studentGrade);
-    printf("Student Score is %f", studentScore);
+    printf("     CALCULATE THE AREA OF RECTANGLE");
+    int lenghtOfTriangle = 5;
+    int widthOfTriangle = 6;
+    int areaOfTriangle = lenghtOfTriangle * widthOfTriangle;
+    printf("Area of Rectangle = lenght x width");
+    printf("if lenght of Rectangle is %d and width of Rectangle is %d", lenghtOfTriangle, widthOfTriangle);
+    printf("Area of Rectangle is %d", areaOfTriangle);
+    return 0;
 }
